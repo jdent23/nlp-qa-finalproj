4c5c0060e5bc736cbb646417b40e6b07cab1be03
Jason <jdent23@utexas.edu>
2021-04-28 20:22:29 -0500

    Finishes project

--- collect_data.py
+++ collect_data.py
@@ -3,24 +3,28 @@ import ast
 
 
 def collect_data():
-    training_models = ["baseline"]
-    steps = 11
+    training_models = ["ensembling", "baseline"] # ["baseline"]
+    num_steps = 21
+    max = 1
+    min = 0.5
+    steps = range(num_steps-1, 0-1, -1)
     
     for training_model in training_models:
-        for hdr in range(0, steps):
-            hdr /= (steps-1)
+        for hdr in steps:
+            hdr = min + hdr / (num_steps-1) * (max - min)
 
-            cmd = "python3 " + training_model + ".py --hidden_data_ratio " + str(hdr) + " --do_train"
+            cmd = "python3 -W ignore " + training_model + ".py --hidden_data_ratio " + str(hdr) + " --do_train"
             print("running: " + cmd)
 
             p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
             out, err = p.communicate()
 
 
+
     print("model\thdr\tEM\tF1")
     for training_model in training_models:
-        for hdr in range(0, steps):
-            hdr /= (steps-1)
+        for hdr in steps:
+            hdr = min + hdr / (num_steps-1) * (max - min)
 
             predictions_name = training_model + "_w_hdr_" + str(hdr) + ".txt"
             cmd = "python3 evaluate.py --output_path ./predictions/" + training_model + "_w_hdr_" + str(hdr) + ".txt"
